d486131dfcfdd73e2b1b6e342a43b13ca7d62b13
GabS. <gabrielsoares1602@gmail.com>
2024-05-17 22:31:14 -0300

    add main container and cards

--- src/MainContentSheet/index.tsx
+++ src/MainContentSheet/index.tsx
@@ -3,7 +3,44 @@ import './index.scss';
 function MainContentSheet() {
   return (
     <div className='container-sheet'>
-      <h2>status do personagem</h2>
+      <div className="container-status">
+        <div className="container-name">
+          <span>Status do personagem</span>
+        </div>
+        <div className="container-content">
+          <div className="chac-pic">
+            <img src="./src/img/Cassandra.png" alt="Portrait do personagem" />
+          </div>
+          <div className="name-hp">
+            <div className="chac-name">
+              Cassandra Di Angelo
+            </div>
+            <div className="chac-hp">
+              <div className="chac-hp-bar">
+                <span>VIDA:</span>
+              </div>
+              <div className="chac-hp-bar">
+
+              </div>
+              <div className="chac-hp-bar">
+
+              </div>
+            </div>
+          </div>
+          <div className="atributos">
+            123123
+          </div>
+          <div className="lvl-status">
+            12312312
+          </div>
+          <div className="base-status">
+            123123
+          </div>
+          <div className="protect-status">
+            1231231
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
